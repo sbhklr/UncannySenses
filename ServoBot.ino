5ab708d5ca6a84939262e9e8a4f9fa0057b7615e
Sebastian Hunkeler <hunkeler.sebastian@gmail.com>
2017-03-09 19:26:31 +0100

    Integrated Flora LED

--- ServoBot.ino
+++ ServoBot.ino
@@ -1,5 +1,11 @@
 #include <Servo.h> 
-//#include "Random.h"
+#include "Adafruit_NeoPixel.h"
+
+//Flora Pixel Defines
+#define AMOUNT_OF_FLORA_PIXELS 1
+#define FLORA_LED_PIN 8
+#define FLORA_MAX_BRIGHTNESS 255
+#define FLORA_BRIGHTNESS_STEP_SIZE 10
 
 #define PIR_PIN 7
 #define IR_PIN A0
@@ -9,6 +15,8 @@
 #define BAUD_RATE 9600
 
 #define SLEEP_TIMEOUT 8
+
+#define HEAD_LOOKUP_ANGLE 150
 
 #define HEAD_SLEEP_ANGLE 60
 #define FEET_SLEEP_ANGLE 90
@@ -23,16 +31,21 @@ Servo servoFeet;
 Servo servoHead;
 Servo servoCalibration;
 
+Adafruit_NeoPixel pixels = Adafruit_NeoPixel(AMOUNT_OF_FLORA_PIXELS, FLORA_LED_PIN, NEO_GRB + NEO_KHZ800);
+
 bool isAwake = true;
 unsigned long lastAwake = 0;
+int currentLEDBrightness = 0;
+int pulsateDirection = 1;
 
 enum IRProximity {
-  CloseProximity = 6,
-  MediumProximity = 10,
+  CloseProximity = 4,
+  MediumProximity = 6,
   FarProximity = 20
 };
 
 void setupPins(){
+  pinMode(FLORA_LED_PIN, OUTPUT);
   pinMode(PIR_PIN, INPUT);
   pinMode(IR_PIN, INPUT);
   pinMode(SERVO_FEET_PIN, OUTPUT);
@@ -43,6 +56,9 @@ void setupPins(){
 void setup() {
   Serial.begin(BAUD_RATE);
   setupPins();
+
+  pixels.begin();
+  pixels.setPixelColor(0, pixels.Color(0,0,0));
   servoFeet.attach(SERVO_FEET_PIN);
   servoHead.attach(SERVO_HEAD_PIN);
   calibrate();
@@ -52,6 +68,13 @@ void setup() {
 void calibrate(){  
   servoCalibration.attach(SERVO_CALIBRATE_PIN);
   servoCalibration.write(180);
+}
+
+void pulsateFloraLED(){
+  currentLEDBrightness = currentLEDBrightness + (FLORA_BRIGHTNESS_STEP_SIZE * pulsateDirection);  
+  pixels.setPixelColor(0, pixels.Color(currentLEDBrightness,0,0));
+  pixels.show();
+  if(currentLEDBrightness + FLORA_BRIGHTNESS_STEP_SIZE > FLORA_MAX_BRIGHTNESS || currentLEDBrightness - FLORA_BRIGHTNESS_STEP_SIZE < 0) pulsateDirection *= -1;
 }
 
 int randomDirection(){
@@ -81,7 +104,7 @@ void setServoPosition(Servo servo, int position){
   servo.write(position);
 }
 
-void shakeHead(int speedPercentage = 100) {
+void shakeHead() {
   int originalFeetPosition = servoFeet.read();
   setServoPosition(servoFeet, originalFeetPosition - 10);
   delay(100);
@@ -89,6 +112,10 @@ void shakeHead(int speedPercentage = 100) {
   delay(100);
   setServoPosition(servoFeet, originalFeetPosition);
   delay(100);
+}
+
+void lookUp(){
+  setServoPosition(servoHead, HEAD_LOOKUP_ANGLE);
 }
 
 bool shouldBackOff(){
@@ -99,6 +126,7 @@ bool shouldBackOff(){
 
 void gotoSleep(){
   if(!isAwake){ return; }
+  Serial.println("Go to sleep.");
   setServoPosition(servoFeet, FEET_SLEEP_ANGLE);
   setServoPosition(servoHead, HEAD_SLEEP_ANGLE);
   isAwake = false;
@@ -107,6 +135,7 @@ void gotoSleep(){
 void wakeUp(){
   lastAwake = millis();  
   if(isAwake){ return; }
+  Serial.println("Wake up.");
 
   int direction = randomDirection();
   setServoPosition(servoFeet, servoFeet.read() + FEET_WAKEUP_ANGLE * direction);
@@ -115,20 +144,21 @@ void wakeUp(){
 }
 
 void lookAround(){
+  Serial.println("Look around.");
   int originalFeetPosition = servoFeet.read();
   int originalHeadPosition = servoHead.read();
 
   setServoPosition(servoFeet, originalFeetPosition - random(FEET_LOOKAROUND_ANGLE));
-  if(shouldBackOff()){shakeHead(); return;}  
+  if(shouldBackOff()){lookUp(); return;}  
   delay(750);
   setServoPosition(servoFeet, originalFeetPosition + random(FEET_LOOKAROUND_ANGLE));
-  if(shouldBackOff()){shakeHead(); return;}  
+  if(shouldBackOff()){lookUp(); return;}  
   delay(1000);
   setServoPosition(servoFeet, originalFeetPosition);
-  if(shouldBackOff()){shakeHead(); return;}  
+  if(shouldBackOff()){lookUp(); return;}  
   delay(350);
   setServoPosition(servoHead, servoHead.read() + HEAD_LOOKAROUND_ANGLE);
-  if(shouldBackOff()){shakeHead(); return;}  
+  if(shouldBackOff()){lookUp(); return;}  
   delay(1500);
   setServoPosition(servoHead, originalHeadPosition);
 }
@@ -139,6 +169,8 @@ int secondsSinceLastAwakening(){
 }
 
 void loop() {  
+  pulsateFloraLED();
+
   int proximity = calculate_distance(analogRead(IR_PIN));
   Serial.println(proximity);
   
@@ -148,9 +180,9 @@ void loop() {
     lookAround();    
   }
 
-  if(shouldBackOff()){shakeHead(); return;}  
+  if(shouldBackOff()){lookUp(); return;}  
 
   if(secondsSinceLastAwakening() >= SLEEP_TIMEOUT) gotoSleep();
 
-  delay(100);
+  delay(50);
 }
